f84b3ad160234fc5d7f82936fbd89d5a76b32669
Pranav Tartey <pranavtartey@gmail.com>
2024-07-21 02:24:03 +0530

    Added the content of safety page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
   const submitHandler = (event) => {
     event.preventDefault();
     console.log(inputValue);
-  }
+  };
 
   return (
     <>
@@ -62,6 +62,7 @@ const Contact = () => {
           value={inputValue.message}
           onChange={handleChange}
         />
+        <br />
         <button>Submit</button>
       </form>
     </>
--- src/Components/Safety/Safety.jsx
+++ src/Components/Safety/Safety.jsx
@@ -1,5 +1,176 @@
 const Safety = () => {
-  return <p>This is my Safety page</p>;
+  return (
+    <>
+      <div>
+        <h1>Adventure begin with Safety!</h1>
+        <p>
+          Safety is our first priority at The Adventure Buddy. We want to ensure
+          that your experience with us is not only thrilling and adventerous but
+          also secure and well-protected. Our commitment to safety runs deep,
+          and take comprehensive measures to ensure your well-being throughout
+          your journey with us.
+        </p>
+        <p>
+          We use standard gear for all adventure activities, including
+          harnesses, helmets, carabiners, ropes and other equipments. Our gear
+          is of highest quality, sourced from Italy & USA.
+        </p>
+        <p>
+          We ensure student safety through certified equipment, professsional
+          trainers, and comprehensive insurance. Our expert trainers provide the
+          top-notch guidance and emergency response. We prioritize health with
+          on-site first aid and medical supplies. Our comprehensive insurance
+          offers peace of mind, making safety our top priority
+        </p>
+      </div>
+
+      <div>
+        <h1>Certified Equipment</h1>
+        <ul>
+          <li>Only use the equipments that meet the industry standards.</li>
+          <li>
+            Regularly inspect and maintain all gear to ensure it is in good
+            working condition.
+          </li>
+          <li>
+            Provide the proper fitting and adjustment of equipment for each
+            participant.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Professional Trainer</h1>
+        <ul>
+          <li>
+            We employ certified and experienced trainers skilled in both
+            activities and emergency response.
+          </li>
+          <li>
+            Maintain a low trainer-to-student ratio to ensure adequet
+            supervision.
+          </li>
+          <li>
+            Conduct thorough background checks and provide contineous training
+            for staff.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Comprehensive Risk Assesssment</h1>
+        <ul>
+          <li>
+            Conduct a detailed risk assessment of all activities and locations.
+          </li>
+          <li>
+            Identify potential hazards and implement measures to mitigate them.
+          </li>
+          <li>
+            Have an emergency response plan tailored to each activity and
+            locations.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Pre Camp Safety Briefing</h1>
+        <ul>
+          <li>
+            Provide the students and parents with the comprehensive safety
+            briefing before the camp.
+          </li>
+          <li>
+            Explain the rules, emergency procedure, and the importance of
+            following the instructions.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Health And Medical Precautions</h1>
+        <ul>
+          <li>
+            Collect and review the health information and emergency contacts for
+            all participants.
+          </li>
+          <li>
+            Ensure easy access to first ade supplies and have trained first aid
+            personnel on-site.
+          </li>
+          <li>
+            Prepare for specific medical needs, such as allergies or chronic
+            conditions.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Activity Specific Safety Measures</h1>
+        <ul>
+          <li>
+            Ensure activities are age-appropriate and math the skill levels of
+            the participants.
+          </li>
+          <li>Establish clear boundaries and guidelines for each activity.</li>
+          <li>
+            Use spotters and safety lines where necessary, particularly in
+            high-risk activities like climbing.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Environmental Safety</h1>
+        <ul>
+          <li>
+            Monitor the weather conditions and adjust the activities
+            accordingly.
+          </li>
+          <li>Provide adequate hydration and sun protection.</li>
+          <li>Ensure safe and hygienic camping facilities.</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Insurance and Liablity</h1>
+        <ul>
+          <li>
+            Secure comprehensive insurance coverage for all participants and
+            staff.
+          </li>
+          <li>
+            Obtain necessary permissions and waivers from parents or guardians.
+          </li>
+          <li>
+            Clearly communicate the extent of insurance coverage and procedures
+            for the claims.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Continuous Monitoring and Communication</h1>
+        <ul>
+          <li>
+            Implement a buddy system to ensure students are always accounted
+            for.
+          </li>
+          <li>
+            Use communication devices for trainers and staff to stay connected.
+          </li>
+          <li>
+            Conduct the regular headcounts and check-ins throughout the day.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Post Activity Review</h1>
+        <ul>
+          <li>
+            Conduct the debriefing sessions to review safety performance and
+            gather feedback.
+          </li>
+          <li>
+            Make necessary adjustments to imporve safety protocols for future
+            camps.
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Safety;
