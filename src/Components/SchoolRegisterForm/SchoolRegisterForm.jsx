f51a110f54b34968dd0eabc46a59814417cb0c77
Pranav Tartey <pranavtartey@gmail.com>
2024-08-08 02:00:21 +0530

    Added the styling in the School Register/ school signup

--- src/Components/SchoolRegisterForm/SchoolRegisterForm.jsx
+++ src/Components/SchoolRegisterForm/SchoolRegisterForm.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import styles from "./SchoolRegisterForm.module.css";
+import image from "./logo.png"
 
 const SchoolRegisterForm = () => {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     name: "",
     address: "",
+    phoneNumber: "",
     state: "",
     city: "",
     pincode: "",
@@ -31,7 +34,7 @@ const SchoolRegisterForm = () => {
   const submitHandler = async (event) => {
     event.preventDefault();
     const response = await axios.post(
-      `http://localhost:8080/the_adventure_buddy/public/create-school`,
+      "http://localhost:8080/the_adventure_buddy/public/create-school",
       inputValue,
       { withCredentials: true }
     );
@@ -41,74 +44,115 @@ const SchoolRegisterForm = () => {
 
   return (
     <>
-      <h1>School Register</h1>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={inputValue.name}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="address">Address</label>
-        <input
-          type="text"
-          name="address"
-          id="address"
-          value={inputValue.address}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="state">State</label>
-        <input
-          type="text"
-          name="state"
-          id="state"
-          value={inputValue.state}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="city">City</label>
-        <input
-          type="text"
-          name="city"
-          id="city"
-          value={inputValue.city}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={inputValue.email}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={inputValue.password}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="pincode">Pincode</label>
-        <input
-          type="number"
-          name="pincode"
-          id="pincode"
-          value={inputValue.pincode}
-          onChange={handleChange}
-        />
-        <br />
-        <button>Submit</button>
-        {/* <button onClick={printHandler}>Print</button> */}
-      </form>
+      <div className="flex justify-center items-center m-20  ">
+        <div className=" bg-[url('bg.jpg')] p-2 m-1">
+          <div className="flex items-center space-x-8 p-4 shrink-0">
+            <img src={image} alt="" />
+            <button className="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 m-2">
+              Signup
+            </button>
+            <button className="px-3  py-1 bg-blue-500 text-white rounded hover:bg-blue-600 focus:ring-2 focus:ring-blue-400">
+              Login
+            </button>
+          </div>
+
+          <h1 className="text-3xl font-semibold text-center ">
+            Create Your Account
+          </h1>
+          <form className="grid gap-2" onSubmit={submitHandler}>
+            <label htmlFor="name"></label>
+            <input
+              className="rounded px-2 w-96  shadow"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="School Name"
+              value={inputValue.name}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="address"></label>
+            <input
+              className="rounded px-2 w-96  shadow"
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Address"
+              value={inputValue.address}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="state"></label>
+            <input
+              className="rounded px-2 w-96 shadow "
+              type="text"
+              name="state"
+              id="state"
+              placeholder="State"
+              value={inputValue.state}
+              onChange={handleChange}
+            />
+            <label htmlFor="phoneNumber"></label>
+            <input
+              className="rounded px-2 w-96 shadow"
+              type="tel"
+              name="phoneNumber"
+              id="phoneNumber"
+              placeholder="Phone Number"
+              value={inputValue.phoneNumber}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="city"></label>
+            <input
+              className="rounded px-2 w-96 shadow "
+              type="text"
+              name="city"
+              placeholder="City"
+              id="city"
+              value={inputValue.city}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="email"></label>
+            <input
+              className="rounded px-2 w-96 shadow "
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={inputValue.email}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="password"></label>
+            <input
+              className="rounded px-2 w-96 shadow "
+              type="password"
+              placeholder="Password"
+              name="password"
+              id="password"
+              value={inputValue.password}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="pincode"></label>
+            <input
+              className="rounded px-2 w-96 shadow "
+              type="number"
+              placeholder="Pincode"
+              name="pincode"
+              id="pincode"
+              value={inputValue.pincode}
+              onChange={handleChange}
+            />
+
+            <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
